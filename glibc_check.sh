bf5856c4f93b3957beeddf06df718a984d2c52df
antonellocaroli <antonellocaroli@gmail.com>
2021-06-04 06:08:23 +0200

    Create glibc_check.sh

---
+++ glibc_check.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+glib_installed=$(equery --quiet list  glibc | sed 's/sys-libs\/glibc-//')
+gp_model=$(sed -n 16p /etc/default/.hw_model)
+if [ "$gp_model" = "Rpi3-64bit" ] ; then
+  glib_request=2.33
+elif [ "$gp_model" = "Rpi4-64bit" ] ; then
+  glib_request=2.33
+elif [ "$gp_model" = "x86-64bit" ] ; then
+  glib_request=2.33
+elif [ "$gp_model" = "Sparky-UsbBridge" ] ; then
+  glib_request=2.33
+elif [ "$gp_model" = "BBB" ] ; then
+  glib_request=2.33
+fi
+if [ ! "$glib_installed" = "$glib_request" ] ; then
+  echo "no OK"
+  exit 1
+fi
