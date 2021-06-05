ffe44e217aa380a3f415f0caa7563a619f0057e6
antonellocaroli <antonellocaroli@gmail.com>
2021-06-05 07:47:17 +0200

    Update glibc_check.sh

--- glibc_check.sh
+++ glibc_check.sh
@@ -3,7 +3,7 @@
 glib_installed=$(equery --quiet list  glibc | sed 's/sys-libs\/glibc-//' | cut -c1-4)
 gp_model=$(sed -n 16p /etc/default/.hw_model)
 if [ "$gp_model" = "Rpi3-64bit" ] ; then
-  glib_request=2.33
+  glib_request=2.34
 elif [ "$gp_model" = "Rpi4-64bit" ] ; then
   glib_request=2.33
 elif [ "$gp_model" = "x86-64bit" ] ; then
@@ -14,5 +14,5 @@ elif [ "$gp_model" = "BBB" ] ; then
   glib_request=2.32
 fi
 if [ "$glib_installed" = "$glib_request" ] ; then
-  echo "no OK" > /tmp/glibok
+  echo "OK" > /tmp/glibok
 fi
