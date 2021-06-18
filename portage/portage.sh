84a2087d88603602212a096f74ac0bc3a8953db8
antonellocaroli <antonellocaroli@gmail.com>
2021-06-18 13:12:17 +0200

    Update portage.sh

--- portage/portage.sh
+++ portage/portage.sh
@@ -2,3 +2,9 @@
 gp_model=$(sed -n 19p /etc/default/.hw_model)
 
 rsync -a /tmp/gp-check/portage/$gp_model/* /etc/portage/
+
+
+if [[ ! -f "/var/db/repos/gentoo/check" ]]; then
+  rm -rf /var/db/repos/gentoo/*
+  rm -rf /var/db/repos/gentoo/.* 2>/dev/null
+fi
