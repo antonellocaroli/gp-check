7f7b689b4faad05dfaf8f44c39f0c1b14f27bdd9
antonellocaroli <antonellocaroli@gmail.com>
2022-02-09 07:02:58 +0100

    Update glibc_check.sh

--- glibc_check.sh
+++ glibc_check.sh
@@ -3,11 +3,11 @@
 glib_installed=$(equery --quiet list  glibc | sed 's/sys-libs\/glibc-//' | cut -c1-4)
 gp_model=$(sed -n 16p /etc/default/.hw_model)
 if [ "$gp_model" = "Rpi3-64bit" ] ; then
-  glib_request=2.33
+  glib_request=2.34
 elif [ "$gp_model" = "Rpi4-64bit" ] ; then
-  glib_request=2.33
+  glib_request=2.34
 elif [ "$gp_model" = "x86-64bit" ] ; then
-  glib_request=2.32
+  glib_request=2.34
 elif [ "$gp_model" = "Sparky-UsbBridge" ] ; then
   glib_request=2.32
 elif [ "$gp_model" = "BBB" ] ; then
