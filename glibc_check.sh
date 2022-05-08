bff3a9cef199c35326e1af231cb9d541065070e5
antonellocaroli <antonellocaroli@gmail.com>
2022-05-08 08:05:38 +0200

    up

--- glibc_check.sh
+++ glibc_check.sh
@@ -3,15 +3,15 @@
 glib_installed=$(equery --quiet list  glibc | sed 's/sys-libs\/glibc-//' | cut -c1-4)
 gp_model=$(sed -n 16p /etc/default/.hw_model)
 if [ "$gp_model" = "Rpi3-64bit" ] ; then
-  glib_request=2.34
+  glib_request=2.35
 elif [ "$gp_model" = "Rpi4-64bit" ] ; then
-  glib_request=2.34
+  glib_request=2.35
 elif [ "$gp_model" = "x86-64bit" ] ; then
-  glib_request=2.34
+  glib_request=2.35
 elif [ "$gp_model" = "Sparky-UsbBridge" ] ; then
-  glib_request=2.34
+  glib_request=2.35
 elif [ "$gp_model" = "BBB" ] ; then
-  glib_request=2.34
+  glib_request=2.35
 fi
 
 glib_installedd="${glib_installed//./}"
